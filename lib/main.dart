aa81ecdec8eabe01a4e0e3c26dddb35c3d5013a1
aboudmahmoud <aboudmahmoud40@gmail.com>
2022-04-29 01:33:09 +0200

    test 1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        backgroundColor: Colors.pink,
+        body: Container(
+          color: Colors.white,
+          child: Text("Aboud"),
+        ),
+      ),
+        );
+  }
+}
+
+
