b2c429cdb38fd56b646069d2aee76c5555d9fba2
aboudmahmoud <aboudmahmoud40@gmail.com>
2022-04-30 16:25:45 +0200

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,15 +13,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            Icon(Icons.add),
+            Center(child: Container(child: Text("hello"),color: Colors.red,)),
+            TextButton(
+                onPressed: () {
+                  print("hello world");
+                },
+                child: Text('click here')),
+            IconButton(onPressed: (){print("U are Asowme Dud");},
+              icon: Icon(Icons.phone),color: Colors.green,)
+          ],
+          backgroundColor: Colors.purple,
+          leading: Icon(Icons.keyboard),
+          title: Text("Asowme App"),
+        ),
         backgroundColor: Colors.pink,
         body: Container(
           color: Colors.white,
           child: Text("Aboud"),
         ),
       ),
-        );
+    );
   }
 }
-
-
